ba9fb847310cfe236c1720cf8d32827f84ba6667
glcarranzani <glcarranzani@gmail.com>
2022-06-06 16:26:17 -0300

    ejercicio2 array-lista

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
 //ejercicio en hola_mundo.js
+
+//ejercicio dos- array
+    /* Crea un nuevo archivo JS que contenga una lista con los siguientes elementos:
+
+    - Tu nombre (string)
+
+    - Tu edad (number)
+
+    - ¿Eres desarrollador? (boolean)
+
+    - Tu fecha de nacimiento (Date)
+
+    - Tu libro favorito (Objeto con propiedades: titulo, autor, fecha, url) */
+
+const miArray = [
+    'Gianina', 
+    28, 
+    true, 
+    new Date(1993,9,13), 
+    {
+        titulo: 'El despertar de los dragones',
+        autor: 'Morgan Rice',
+        fecha: new Date(2015, 3, 10),
+        url: 'https://play.google.com/store/books/details/El_Despertar_de_los_Dragones_Reyes_y_Hechiceros_Li?id=MbUFCAAAQBAJ&hl=es_AR&gl=US'
+    }
+];
